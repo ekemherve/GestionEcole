8fd0c3df8fe29c70abc6899c6f2f2c9385ee8daf
Ekem herve <ekemherve@gmail.com>
2018-08-31 20:49:11 +0200

    ajout du composant heade + insertion dans app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { RouterModule, Routes} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   {path: 'accueil', component: AccueilComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
