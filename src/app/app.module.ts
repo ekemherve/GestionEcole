645d49de31604e04f7a5151a70ada377ce705424
Ekem herve <ekemherve@gmail.com>
2018-08-31 20:39:05 +0200

    ajout du composant accueil + imports dans app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'accueil', component: AccueilComponent},
+  {path: '', component: AccueilComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
